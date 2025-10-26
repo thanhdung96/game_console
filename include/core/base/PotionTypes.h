343d21ba096217793d6090d718e8f6c293a9b339
thanhdung96 <duduong96@outlook.com>
2025-10-26 23:45:47 +0700

    added potion constants.
    Modifications qui seront validées :
    	nouveau fichier : include/core/base/PotionTypes.h

---
+++ include/core/base/PotionTypes.h
@@ -0,0 +1,28 @@
+namespace Core::Base {
+enum class PotionType {
+  HEALTH_POTION,
+  MANA_POTION,
+  REJUVENATION_POTION,
+  ANTIDOTE_POTION,
+  THAWING_POTION,
+  STAMINA_POTION
+};
+
+enum class HealthPotionSize {
+  MINOR = 30,
+  LIGHT = 60,
+  MEDIUM = 100,
+  GREATER = 180,
+  SUPER = 320
+};
+
+enum class ManaPotionSize {
+  MINOR = 20,
+  LIGHT = 40,
+  MEDIUM = 80,
+  GREATER = 150,
+  SUPER = 250
+};
+
+enum class RejuvenationPotionSize { MINOR = 35, LIGHT = 100 };
+} // namespace Core::Base
