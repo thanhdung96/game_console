5b810d0eca50e61672d0340bdd533bbeec1681b6
thanhdung96 <duduong96@outlook.com>
2025-10-27 21:59:13 +0700

    added script for auto add source files.
    Modifications qui seront validées :
    	modifié :         CMakeLists.txt
    	nouveau fichier : script/source_gen.py
    	nouveau fichier : src/SOURCES.cmake

---
+++ src/SOURCES.cmake
@@ -0,0 +1,12 @@
+# GENERATED - do not edit unless you know what you're doing
+set(MYPROJECT_SOURCES
+  src/main.cpp
+  src/core/base/GameObject.cpp
+  src/core/base/Item.cpp
+  src/core/item/ConsummableItem.cpp
+  src/core/item/QuestItem.cpp
+  src/core/item/WearableItem.cpp
+  src/core/character/NPC.cpp
+  src/core/character/PlayableCharacter.cpp
+  src/utils/UuidV7.cpp
+)
